70f6cab1f575590e4f61a6ccc7f22d2ff9e00cbc
Konstantin Chvilyov <chvilyov.k@iri.center>
2025-10-21 11:24:40 +0300

    Уточнил названия и описания

--- src/ru/ads/AdjacencyMatrixToEdgeList.java
+++ src/ru/ads/AdjacencyMatrixToEdgeList.java
@@ -9,20 +9,31 @@ public class AdjacencyMatrixToEdgeList {
      * Преобразует матрицу смежности в список рёбер.
      * Смежность - это соседство, близость.
      *
-     * @param matrix матрица смежности (n x n)
-     * @return список рёбер в виде пар (i, j), где i < j
+     * @param adjacencyMatrix матрица смежности (n x n)
+     *         = таблица соседства, таблица связи через рёбра.
+     *         Рёбра для понимания смежности можно назвать
+     *              связующими межами(промежутками, промежностями) между вершинами графа.
+     *         где индексы строк и столбцов соответствуют вершинам графа.
+     *         0 значит, что вершина не связана(соседствует, смежна через ребро) с другой вершиной,
+     *         1 значит, что вершина связана(соседствует, смежна через ребро) с другими вершинами
+     * @return список рёбер в виде пар (firstVerticalIndex, secondVerticalIndex),
+     *              где firstVerticalIndex < secondVerticalIndex это номера вершин,
+     *              причём первый определитель меньше второго, чтобы исключить повтор.
+     *         Каждому ребру соответствует пара вершин, с которыми ребро соединено.
      */
-    public static List<int[]> convert(int[][] matrix) {
-        List<int[]> edgeList = new ArrayList<>();
+    public static List<int[]> convert(int[][] adjacencyMatrix) {
+        List<int[]> edgesList = new ArrayList<>();
 
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = i + 1; j < matrix[i].length; j++) {
-                if (matrix[i][j] == 1) {
-                    edgeList.add(new int[]{i, j});
+        //Определитель первой вершины
+        for (int firstVerticalIndex = 0; firstVerticalIndex < adjacencyMatrix.length; firstVerticalIndex++) {
+            //Определитель второй вершины, больше определителя первой вершины
+            for (int secondVerticalIndex = firstVerticalIndex + 1; secondVerticalIndex < adjacencyMatrix[firstVerticalIndex].length; secondVerticalIndex++) {
+                if (adjacencyMatrix[firstVerticalIndex][secondVerticalIndex] == 1) {
+                    edgesList.add(new int[]{firstVerticalIndex, secondVerticalIndex});
                 }
             }
         }
 
-        return edgeList;
+        return edgesList;
     }
 }
